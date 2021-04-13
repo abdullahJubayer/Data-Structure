32101f66c7d2cd6845dd3b24e22729df47efe1c3
Abdullah jubayer <abdullahjubayer38@gmail.com>
2021-04-14 04:32:28 +0600

    stack balance string prob solve (my solution)

---
+++ src/Stack/Main.java
@@ -0,0 +1,65 @@
+package Stack;
+
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "abcd";
+        String str2 = "[{(1+2)}]{}";
+
+        System.out.println(reverseString(str));
+        System.out.println(isStringBalanced(str2));
+    }
+
+    public static StringBuffer reverseString(String str) {
+        StringBuffer reverse = new StringBuffer("");
+        Stack<String> stack = new Stack<>();
+
+        char[] ch = str.toCharArray();
+        for (char c : ch) {
+            stack.push(String.valueOf(c));
+        }
+        while (!stack.isEmpty()) {
+            reverse.append(stack.pop());
+        }
+        return reverse;
+    }
+
+    public static boolean isStringBalanced(String str) {
+        int flag = 0;
+        Stack<Character> characters = new Stack<>();
+        for (Character c : str.toCharArray()) {
+            if (c.equals('(') || c.equals('{') || c.equals('[') || c.equals('<')) {
+                characters.push(c);
+            } else {
+                if (c.equals(')') || c.equals('}') || c.equals(']') || c.equals('>')) {
+                    if (characters.isEmpty())
+                        return false;
+                    var openingTag = characters.peek();
+                    Character closingTag = '0';
+                    switch (openingTag) {
+                        case '(':
+                            closingTag = ')';
+                            break;
+                        case '{':
+                            closingTag = '}';
+                            break;
+                        case '[':
+                            closingTag = ']';
+                            break;
+                        case '<':
+                            closingTag = '>';
+                            break;
+                    }
+                    if (closingTag.equals(c)) {
+                        flag = 1;
+                        characters.pop();
+                    } else {
+                        flag = 0;
+                    }
+                }
+            }
+        }
+        return flag == 1;
+    }
+}
